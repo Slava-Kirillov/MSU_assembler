ca3dce1a2d7a329cb5f211f8ec2a8811c53fb06f
Slava Kirillov <specctral@gmail.com>
2017-05-03 11:56:29 +0300

    Changes to be committed: 	new file:   a.asm
     Untracked files:
    	a.exe
    	a.lst
    	makeit.bat

---
+++ a.asm
@@ -0,0 +1,99 @@
+include console.inc
+
+.data
+	x db 100 dup (?)
+
+.code
+
+input proc
+    lea edi, x
+    xor ecx,ecx
+      
+icycle:
+    inchar al
+    cmp al,'.'
+    je doneinput       ;ввод окончен
+
+    inc ecx
+    cmp ecx,100
+    je toolong         ;длина строки больше 100 символов
+
+    mov [edi], al
+    inc edi
+    jmp icycle
+
+toolong:
+    outstrln "¬ведено более 100 символов"              ;вывод сообщени€ об ошибке
+    mov eax,1            ;eax=1 - ошибка
+    ret
+
+doneinput:
+    cmp ecx,0
+    je tooshort        ;пуста€ строка
+    dec edi
+    xor eax,eax           ;eax=0 - ошибки нет
+    ret
+
+tooshort:
+    outstrln "¬ведена пуста€ строка"
+    mov eax,1            ;eax=1 - ошибка
+    ret
+
+input endp
+
+printtext proc
+	lea esi, x
+pcycle:
+	mov al, [esi]
+	outchar al
+	inc esi
+	cmp esi, edi
+	jle pcycle
+	newline
+	ret
+printtext endp
+
+check proc
+	mov edx, [edi]
+	call islatin
+
+	ret
+ise:
+	outstrln "явл€етс€ E"
+	ret
+check endp
+
+islatin 
+	
+
+start:
+	outstrln "¬ведите символы, не более 100:"
+	
+	call input      ;ввод текста
+    cmp eax,0
+    jne finish
+  	
+	;lea dx,strtxt ; »сходный текст
+    call printtext   ;вывод введенного текста
+
+    call check      ;проверка свойства
+comment*
+    cmp ax,0
+    je .rule2
+
+.rule1:
+    call rule1      ;преобразование по правилу 1
+    jmp .output
+
+.rule2:
+    call rule2      ;преобразование по правилу 2
+    jmp .output
+
+.output:
+    lea dx,strresult
+    call printstr
+*
+
+finish:
+	exit
+	end start
